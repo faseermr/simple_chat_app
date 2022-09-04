b38fdac2b4e85bae9a465cd128edfadaeb7675be
unknown <studentreesaf@gmail.com>
2022-09-04 09:55:48 +0530

    added to readme file

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const { addUsers, getUser, removeUser, getRoomUsers } = require("./user/user");
 //instance
 const app = express();
 const server = http.createServer(app);
@@ -63,49 +64,5 @@ io.on("connect", (socket) => {
     console.log("Disconnect");
   });
 });
-
-// handle users
-let users = [];
-
-const addUsers = ({ id, username, room }) => {
-  console.log("details", id, username, room);
-  username = username.trim().toLowerCase();
-  room = room.trim().toLowerCase();
-
-  // check username and room is empty
-  if (!username || !room) {
-    return { error: "Name and Room are required." };
-  }
-
-  // check username and room already exists
-  if (users.length) {
-    const data = users.find((e) => e.username === username && e.room === room);
-    console.log("data", data);
-    if (data) {
-      return { error: "user already exist" };
-    }
-  }
-  const response = { id, username, room };
-
-  users.push(response);
-
-  return { response };
-};
-
-const getUser = (id) => {
-  return users.find((e) => e.id == id);
-};
-
-const removeUser = (id) => {
-  const findIdx = users.findIndex((user) => user.id == id);
-
-  if (findIdx >= 0) {
-    return users.splice(findIdx, 1)[0];
-  }
-};
-
-const getRoomUsers = (room) => {
-  return users.filter((e) => e.room === room);
-};
 
 server.listen(5000, () => console.log("Server Started....."));
---
+++ README.md
@@ -0,0 +1,11 @@
+This is simple chat application using React, Node, Express and socket.io
+
+Follow these steps to run the app on your local machine,
+
+Assume you have node & mysql installed
+
+    1> Run "npm install" from the Backend directory & Run "npm install" from the chat-app directory.
+
+    2> To run the front-end and back-end
+        ->  "npm start" on Backend directory
+        ->  "npm start" on chat-app directory
